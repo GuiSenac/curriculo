748e39335f253e2ff518b941c17a1416c9b694b7
GuiSenac <guilherme.ltavares@senacsp.edu.br>
2024-12-18 07:13:03 -0300

    header body and footer

--- app/page.js
+++ app/page.js
@@ -4,7 +4,20 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-     
+     <header>
+      <div className="name" >
+          <h1>Guilherme lopes</h1>
+          </div>
+     </header>
+
+    <body>
+
+    </body>
+
+    <footer>
+
+    </footer>
+
     </div>
   );
 }
